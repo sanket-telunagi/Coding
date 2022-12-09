670470d7a87dbf6e2d92ed989d44125b64eb9e90
kaliuser <telunagisanket@gmail.com>
2022-12-09 19:37:29 +0530

    changes

--- C2_Ladder/OddSum.cpp
+++ C2_Ladder/OddSum.cpp
@@ -11,7 +11,7 @@ int main()
         cin >> n ;
         int cnt = 0;
         multiset<int> v ;
-        multiset<int> :: iterator it1, it2 ;
+        unordered_map<int,int> m  ;
         for (int i = 0; i < 2*n; i++)
         {
             int x ;
@@ -19,16 +19,8 @@ int main()
             v.insert(x);
         }
         
-        for (it1 = v.begin() ; it1 != v.end() ; ++it1)
-        {
-            for (it2 = it1; it2 != v.end() ; ++it2) {
-                if ((*it1 + *(++it2)) != 0) {
-                    v.erase(v.find(*(it1))) ;
-                    v.erase(v.find(*(++it2))) ;
-                    cnt ++ ;
-                }
-            }
-        }
+        
+       
         if (cnt == n)
         {
             cout << "Yes" << endl;
