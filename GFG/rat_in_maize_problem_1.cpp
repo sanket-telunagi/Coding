46bb7f9cb5f907d53d126f8f499b451f8dc5b7ba
Pradnyavant <pradnyavantprogrammer@gmail.com>
2022-12-31 04:12:58 +0530

    "solved more problems"

--- GFG/rat_in_maize_problem_1.cpp
+++ GFG/rat_in_maize_problem_1.cpp
@@ -2,8 +2,74 @@
 #include <iostream>
 using namespace std ;
 
+/*
+    take input as a graph and apply dfs on it 
+*/
 
+vector<bool> visited ;
 
+void findPath(int vertex ,int parent ,  unordered_map<int, unordered_set<int>> & graph, vector<vector<int>> & ans, vector<int> & path){
+    if(vertex == graph.size() - 1) {
+        ans.push_back(path) ;
+        return ;
+    }
+    visited[vertex] = true ;
+    for( auto child : graph[vertex]) {
+        if(child == parent) continue ;
+        if(visited[parent]) continue;
+        path.push_back(child) ;
+        cout << child << " " << vertex << endl  ;
+        findPath(child, vertex, graph, ans, path) ;
+        path.pop_back() ;
+    }
+}
+
+int main() {
+    int n ;
+    cin >> n ;
+
+    unordered_map<int,unordered_set<int>> graph ;
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            int x ;
+            cin >> x ;
+            if (x) {
+                graph[i].insert(j) ;
+                graph[j].insert(i) ;
+            }
+        }
+        
+    }
+
+    vector<vector<int>> ans ;
+
+    vector<int> path ;
+    path.push_back(0) ;
+
+    // findPath(0, 0, graph, ans, path) ;
+
+    // for( auto & it : ans ) {
+    //     for (int & i : it) {
+    //         cout << i << " " ;
+    //     }
+    //     cout << endl ;
+    // }
+    // cout << endl ;
+    for(auto & pr : graph) {
+        cout << pr.first << " : " ;
+        for(int i : pr.second) {
+            cout << i << " " ;
+        }
+        cout << endl ;
+    }
+    
+    return 0 ;
+}
+
+/*
 void genPaths(vector<int> arr [], vector<string> & ans, vector<vector<int>> & path_hsh,string & path,  int i = 0 , int j = 0){
     if(i < 0 || j < 0 || i >= path_hsh.size() || j >= path_hsh[0].size() || arr[i][j] == 0) return ;
     if(i == path_hsh.size() - 1 && j == path_hsh.size() - 1) {
@@ -73,3 +139,4 @@ int main()
 
     return 0 ;
 }
+*/
--- LeetCode/70_Climbing_stairs.cpp
+++ LeetCode/70_Climbing_stairs.cpp
@@ -2,21 +2,48 @@
 #include <iostream>
 using namespace std ;
 
-int climbStairs(int n) {
-    int ct = 0 ;
-    if (n <= 0) return 0;
-    ct = climbStairs(n-1) + 1;
-    if (n - 1 > 1) {
-        ct = climbStairs(n - 2) + 1;
+// int climbStairs(int n) {
+//     int ct = 0 ;
+//     if (n <= 0) return 0;
+//     ct = climbStairs(n-1) + 1;
+//     if (n - 1 > 1) {
+//         ct = climbStairs(n - 2) + 1;
+//     }
+//     return ct ;
+// }
+
+int climbStairs(int n , vector<int> & dp){
+    if (n == 0) return 0 ;
+    if (dp[n] != -1) return dp[n] ;
+    int step = INT_MAX ;
+    step = min(step,climbStairs(n - 1, dp) + 1) ;
+    if(n > 1) {
+        step = min(step, climbStairs(n - 2, dp) + 1) ;
     }
-    return ct ;
+    return dp[n] = step ;
+}
+
+int climbStairs(int n , int * dp){
+    if (n == 0 || n == 1) return 1;
+    if (dp[n] != -1) return dp[n] ;
+    int step = INT_MAX ;
+    step = min(step,climbStairs(n - 1, dp) + 1) ;
+
+    if(n > 1) {
+        step = min(step, climbStairs(n - 2, dp) + 1) ;
+    }
+    return dp[n] = step ;
 }
 
 int main()
 {
     int n ;
     cin >> n ;
-    int ans = climbStairs(n) ;
+    // vector<int> dp(n+1,-1) ;
+    int dp [n+1] ;
+    memset(dp, -1 , sizeof(dp)) ;
+
+    int ans = climbStairs(n,dp) ;
     cout << ans ;
     return 0 ;
 }
