84b64f84cb6e930f664ad4ec794c1eae119f1c22
Pradnyavant <pradnyavantprogrammer@gmail.com>
2023-01-19 00:35:25 +0530

    Java On the Way

---
+++ Practice/first.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class first {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner (System.in) ;
+        int x = sc.nextInt() ;
+        sc.close();
+        System.out.println(x);
+    }
+}
