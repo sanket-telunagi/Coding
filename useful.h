71040518d20f4c8eb764417de43eddd49d2c377c
Pradnyavant <pradnyavantprogrammer@gmail.com>
2023-02-15 22:47:06 +0530

    the 76

--- useful.h
+++ useful.h
@@ -1,14 +1,16 @@
-# include <iostream>
-# include <vector>
+#include <vector>
+// void printG(vector<vector<int>> & G) {
+//     int i = 0 ;
+//     for (auto & it : G) {
+//         cout << i++ << " : " ;
+//         for (auto & i : it ) {
+//             cout << i << " " ;
+//         }
+//         cout << endl ;
+//     }
+// }
 
-void printG(vector<vector<int>> & G) {
-    int i = 0 ;
-    for (auto & it : G) {
-        cout << i << " : " ;
-        for (auto & i : it ) {
-            cout << i << " " ;
-        }
-        cout << endl ;
-        i++ ;
-    }
-}
+// void printV(vector<int> & V) {
+//     for(auto & i : V) cout << i << " " ;
+//     cout << endl ;
+// }
