c75d4c1949cd52099b2c774f7d8564c19dadb426
Pradnyavant <pradnyavantprogrammer@gmail.com>
2023-06-12 21:28:18 +0530

    solved problems on sliding window

--- LeetCode/LeetCode_118_Pascals_triangle.cpp
+++ LeetCode/LeetCode_118_Pascals_triangle.cpp
@@ -2,14 +2,38 @@
 #include <iostream>
 using namespace std ;
 
-unordered_map<int,vector<int>> mp ;
 
-void pascalsTrinagle(int numRows, vector<vector<int>> & ans) {
-    if(numRows == 1) return 1 ;
+void intToDigits(vector<int> & nums, long long num) {
+    while (num > 0) {
+        nums.push_back(num%10) ;
+        num /= 10 ;
+    }
 }
 
 vector<vector<int>> generate(int numRows) {
-        
+    vector<vector<int>> res = {{1}};
+    if (numRows == 0) return {{}} ;
+
+    if (numRows == 2) return {{1}, {1,1}} ;
+
+    else if (numRows > 2) {
+        int ct = 2;
+        res.push_back({1,1}) ;
+        for(int i = ct ; i < numRows; i++) {
+            vector<int> temp ;
+            temp.push_back(1) ;
+            for (int j = 0 ; j < res[i-1].size() - 1; j++) {
+                int num = res[i-1][j] + res[i-1][j+1] ;
+                // cout << num << endl ;
+                temp.push_back(num) ;
+            } 
+            temp.push_back(1) ;
+            res.push_back(temp) ;
+            // cout << res.size() << endl ;
+        }
+    }
+    
+    return res ;
 }
 
 int main()
--- LeetCode/LeetCode_3_Longest_Substring_Without_Repeating_Characters.cpp
+++ LeetCode/LeetCode_3_Longest_Substring_Without_Repeating_Characters.cpp
@@ -8,25 +8,52 @@
 using namespace std ;
 
 int lengthOfLongestSubstring(string s) {
-    int ans = INT_MIN ;
+    // int ans = INT_MIN ;
+    // int ct = 0 ;
+    // unordered_map<char,int> mp ;
+    // for (int i = 0; i < s.length(); i++)
+    // {
+    //     mp[s[i]]++ ;
+    //     if (i == 0) ct++ ;
+    //     // ct++ ;
+    //     else {
+    //         if(s[i] != s[i-1] && (mp[s[i]] == 1)) {
+    //             ct++ ;
+    //         }else {
+    //             ct = 0 ;
+    //             mp[s[i]] = 0 ;
+    //         }
+    //     }
+    //     ans = max(ans, ct) ;
+    // }
+    // return ans ;
+
     int ct = 0 ;
     unordered_map<char,int> mp ;
-    for (int i = 0; i < s.length(); i++)
+    int l = 0 , n = s.length();
+    int r = 0 ;
+    while (r < n)
     {
-        mp[s[i]]++ ;
-        if (i == 0) ct++ ;
-        // ct++ ;
-        else {
-            if(s[i] != s[i-1] && (mp[s[i]] == 1)) {
-                ct++ ;
-            }else {
-                ct = 0 ;
-                mp[s[i]] = 0 ;
+        mp[s[r]]++ ;
+        // if the window size equals to the size of map 
+        // which eventually means that the map conatines all unique elements 
+        if (mp.size() == r - l + 1) {
+            // cout << s.substr(l, r+1) << endl  ;
+            ct = max(ct, r - l + 1) ;
+            r++ ;
+        } else if (mp.size() < r - l + 1) {
+            while (mp.size() < r - l + 1) {
+                mp[s[l]] -- ;
+                if(mp[s[l]] == 0) {
+                    mp.erase(s[l]) ;
+                }
+                l++ ;
             }
+            r++ ;
         }
-        ans = max(ans, ct) ;
     }
-    return ans ;
+    return ct ;
+    
 }
 
 int main()
