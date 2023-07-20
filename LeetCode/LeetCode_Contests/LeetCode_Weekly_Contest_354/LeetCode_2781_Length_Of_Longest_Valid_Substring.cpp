2c94ee7747a1f1d7a9dcc01d12b318ca4ee09bbd
Pradnyavant <pradnyavantprogrammer@gmail.com>
2023-07-21 00:32:14 +0530

    codechef contests

--- LeetCode/LeetCode_Contests/LeetCode_Weekly_Contest_354/LeetCode_2781_Length_Of_Longest_Valid_Substring.cpp
+++ LeetCode/LeetCode_Contests/LeetCode_Weekly_Contest_354/LeetCode_2781_Length_Of_Longest_Valid_Substring.cpp
@@ -3,30 +3,57 @@
 using namespace std ;
 
 int longestValidSubstring(string word, vector<string>& forbidden) {
-    int n = word.length() ;
-    unordered_map<string, int> mp ;
-    for(auto & it : forbidden) mp[it]++ ;
+    // int n = word.length() ;
+    // unordered_map<string, int> mp ;
+    // for(auto & it : forbidden) mp[it]++ ;
 
-    int l = 0 , r = 0 , res = 0  ;
+    // int l = 0 , r = 0 , res = 0  ;
 
-    for (int i = 0; i < n; i++)
-    {
-        string temp = word.substr(l, r - l + 1) ;
-        cout << temp << endl ;
-        if (mp.find(temp) != mp.end()) {
-            while (mp.find(temp) != mp.end()) {
-                l++ ;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     string temp = word.substr(l, r - l + 1) ;
+    //     cout << temp << endl ;
+    //     if (mp.find(temp) != mp.end()) {
+    //         while (mp.find(temp) != mp.end()) {
+    //             l++ ;
                 
-                temp = word.substr(l, r - l + 1) ;
-                cout << temp << endl ;
+    //             temp = word.substr(l, r - l + 1) ;
+    //             cout << temp << endl ;
+    //         }
+    //     }
+    //     res = max(res, r - l + 1) ;
+    //     r++ ;
+    // }
+
+    // return res ;
+
+    int n = word.length() , 
+        r = n - 1 ,
+        l = r ,
+        res = 0 ;
+    /*
+        Require sol : 
+            TC : O(n) / O(nlog(n))
+        
+        Use the constraint :
+            forbidden[i].length <= 10
+    */
+
+    unordered_set<string> st ;
+    for (auto & it : forbidden) st.insert(it) ;
+    for (l = n - 1; l >= 0; l--)
+    {
+        for (int k = l; k <= min(l + 10, r); k++)
+        {
+            string temp = word.substr(l, k - l + 1) ;
+            if (st.find(temp) != st.end()) {
+                r = k - 1 ;
+                break;
             }
         }
         res = max(res, r - l + 1) ;
-        r++ ;
     }
-
     return res ;
-    
 }
 
 int main()
