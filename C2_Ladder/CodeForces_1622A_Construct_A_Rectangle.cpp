58ae266bba827879c58aa40c27ccae2d37547ecf
Sanket Telunagi <sankette@amdocs.com>
2025-01-04 23:17:14 +0530

    code changes

--- C2_Ladder/CodeForces_1622A_Construct_A_Rectangle.cpp
+++ C2_Ladder/CodeForces_1622A_Construct_A_Rectangle.cpp
@@ -25,7 +25,7 @@ int main()
     cin >> t;
     while (t--)
     {
-        vector<int> nums(3, 0);
+        vector<int> nums(4, 0);
         for (int i = 0; i < 3; i++)
         {
             cin >> nums[i];
